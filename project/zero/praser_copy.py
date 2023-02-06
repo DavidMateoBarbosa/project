c715d872efbfffa1caec177958a775776186f07b
David Mateo Barbosa Monsalve <d.barbosam@uniandes.edu.co>
2023-02-05 22:43:11 -0500

    command interpreter first steps

--- project/zero/praser_copy.py
+++ project/zero/praser_copy.py
@@ -10,7 +10,7 @@ class _Parser:
     available_commands: dict[str, Callable[[Self, Any], Any]]
     def __init__(self, file: TextIOWrapper) -> None:
         self.logic = LogicAndGraphic()
-        self.script = list(file)
+        self.script = list(file)[1:]
         self.vars = {}
         self.available_commands = {
             'assingTo': self.assingTo,
@@ -30,40 +30,42 @@ class _Parser:
     def _parser(self) -> None:
         _is_in_commands = False
         for line in self.script:
-            print(line)
-            if line == "[" or line.startswith("["):
-                if line.startswith("[ ") or line.startswith(" "):
-                    raise TypeError()
+            if line.startswith(" "):
+                raise TypeError()
+            if line.replace(" ", "").replace("\n", "") == "[" or line.startswith("["):
                 _is_in_commands = True
+            elif line.startswith("[ "):
+                raise TypeError()
             if _is_in_commands:
-                print(line)
                 self._commands(line)
-            else:
-                pass
-
+    
     def _commands(self, line: str) -> None:
-        line = line.replace(" ","")
-        if line.startswith("["):
-            line = line[1:]
-        if line.endswith("]"):
-            line = line[:-1]
-        sentinel_1 = line.endswith(";\n")
-        sentinel_2 = line.endswith("\n;")
-        print(sentinel_1, sentinel_2)
-        if (not sentinel_1) and (not sentinel_2):
-            raise TypeError()
+        if line.replace(" ", "").replace("\n", "") == "[" or line.replace(" ", "").replace("\n", "") == "]": return
+        line = line.replace(" ", "").replace("\n", "")
+        if not line.endswith(";"): raise TypeError()
+        line = line[:-1]
+        function, params = line.split(":")
+        params = params.split(",")
+        if function == 'put' or function == 'pick': eval(f"self.{function}({params[0]}, '{params[1]}')")
+        else: eval(f"self.{function}({params[0]}, {params[1]})")
         
 
 
-        print(line)
-
     def assingTo(self, n) -> ...: ...
-    def goto(self, x, y) -> ...: ...
-    def move(self, n) -> ...: ...
+    def goto(self, x, y) -> ...:
+        self.logic.go_to((x, y))
+    def move(self, n) -> ...:
+        for _ in range(n):self.logic.move_forward()
     def turn(self, D) -> ...: ...
     def face(self, O) -> ...: ...
-    def put(self, n, item) -> ...: ...
-    def pick(self, n, item) -> ...: ...
+    def put(self, n, item) -> ...: 
+        for _ in range(n):
+            if item == "Balloon":self.logic.put_balloon()
+            elif item == "Chip":self.logic.put_chip()
+    def pick(self, n, item) -> ...:
+        for _ in range(n):
+            if item == "Balloon":self.logic.pick_balloon()
+            elif item == "Chip":self.logic.pick_chip()
     def moveToThe(self, n, D) -> ...: ...
     def moveInDir(self, n, O) -> ...: ...
     def jumpToThe(self, n, D) -> ...: ...
@@ -72,14 +74,9 @@ class _Parser:
 
 
 def parser(file: str) -> Literal["script and execution good request", "script good request, execution bad request", "script bad request"]:
-    with open(file, mode='r', encoding='utf-8') as file:
-            parse = _Parser(file)
-            print(parse.logic.first_last_state())
     try:
-        print(file)
         with open(file, mode='r', encoding='utf-8') as file:
             parse = _Parser(file)
-            print(parse.logic.first_last_state())
         return "script and execution good request"
     except InvalidCommand:
         return "script good request, execution bad request"
